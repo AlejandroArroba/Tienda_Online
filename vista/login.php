a9fea0d0a0b024e4b6feec947be815de4368dc41
Alejandro Arroba <alejandroarroba2001@gmail.com>
2024-11-07 11:56:06 +0100

    Actualizado Login y Controlador

--- vista/login.php
+++ vista/login.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+</head>
 <body>
-<h1>Inicio de Sesión</h1>
-<form action="../Controlador/ControladorLogin.php">
-    <label>Nombre de Usuario</label>
-    <label for="nickname"></label><input type="text" id="nickname" name="nickname" required> <br>
-    <label>Contraseña</label>
-    <label for="contraseña"></label><input type="password" id="contraseña" name="contraseña" required><br><br>
-    <button id="login" name="login">Iniciar Sesión</button><br><br>
 
-    <label>¿No tienes cuenta? <a href="registro.php">Resgistrate aquí</a></label>
+<h1>Inicio de Sesión</h1>
+
+<form action="../controlador/ControladorLogin.php" method="post">
+    <label for="nickname">Nombre de Usuario:</label>
+    <input type="text" id="nickname" name="nickname">
+    <br><br>
+
+    <label for="contraseña">Contraseña:</label>
+    <input type="password" id="contraseña" name="contraseña">
+    <br><br>
+
+    <button type="submit" id="login" name="login">Iniciar Sesión</button>
+    <br><br>
+
+    <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
 </form>
+
 </body>
 </html>
