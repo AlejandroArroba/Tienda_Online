bde59680736992030235eed8a5c91ffd4b8df146
Aenix <damiantren@gmail.com>
2024-11-11 13:56:08 +0100

    Enlazado style.css en index.php y login.php. Reformateado style.css.

--- vista/index.php
+++ vista/index.php
@@ -6,38 +6,38 @@ $productos = $productoDAO->getAllProductos();
 
 <!doctype html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inicio</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="../resources/style.css">
-</head>
-<body>
-<div class="header">
-    Inicio
-    <a href="compra.php" class="cart-icon"><i class="fa-solid fa-cart-shopping"></i></a>
-</div>
-<div class="navbar">
-    <a href="buscar.php">Buscar Producto</a>
-    <a href="anadir.php">Añadir Productos</a>
-    <a href="update.php">Actualizar Productos</a>
-    <a href="delete.php">Borrar Productos</a>
-</div>
-<h1>Lista de productos</h1>
-<table>
-    <tr>
-        <th>Nombre</th>
-        <th>Descripcion</th>
-        <th>Precio</th>
-    </tr>
-    <?php foreach ($productos as $producto) { ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Inicio</title>
+        <link rel="stylesheet" href="../resources/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    </head>
+    <body>
+    <div class="header">
+        Inicio
+        <a href="compra.php" class="cart-icon"><i class="fa-solid fa-cart-shopping"></i></a>
+    </div>
+    <div class="navbar">
+        <a href="buscar.php">Buscar Producto</a>
+        <a href="anadir.php">Añadir Productos</a>
+        <a href="update.php">Actualizar Productos</a>
+        <a href="delete.php">Borrar Productos</a>
+    </div>
+    <h1>Lista de productos</h1>
+    <table>
         <tr>
-            <td><?= $producto->getNombre() ?></td>
-            <td><?= $producto->getDescripcion() ?></td>
-            <td><?= $producto->getPrecio() ?></td>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
         </tr>
-    <?php } ?>
-</table>
+        <?php foreach ($productos as $producto) { ?>
+            <tr>
+                <td><?= $producto->getNombre() ?></td>
+                <td><?= $producto->getDescripcion() ?></td>
+                <td><?= $producto->getPrecio() ?></td>
+            </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
--- vista/login.php
+++ vista/login.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio de Sesión</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inicio de Sesión</title>
+        <link rel="stylesheet" href="../resources/style.css">
+    </head>
+    <body>
+        <h1>Inicio de Sesión</h1>
+        <form action="../controlador/ControladorLogin.php" method="post">
+            <label for="nickname">Nombre de usuario:</label>
+            <input type="text" id="nickname" name="nickname">
+            <br><br>
 
-<h1>Inicio de Sesión</h1>
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password">
+            <br><br>
 
-<form action="../controlador/ControladorLogin.php" method="post">
-    <label for="nickname">Nombre de usuario:</label>
-    <input type="text" id="nickname" name="nickname">
-    <br><br>
+            <input type="submit" id="login" name="login" value="Inicar Sesión">
+            <br><br>
 
-    <label for="password">Contraseña:</label>
-    <input type="password" id="password" name="password">
-    <br><br>
-
-    <input type="submit" id="login" name="login" value="Inicar Sesión">
-    <br><br>
-
-    <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
-    <?php
-        session_start();
-        if (isset($_SESSION["errorLogin"])) {
-            echo "<p class='error'>$_SESSION[errorLogin]</p>";
-            unset($_SESSION["errorLogin"]);
-        }
-    ?>
-
-</form>
-
-</body>
+            <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
+            <?php
+                session_start();
+                if (isset($_SESSION["errorLogin"])) {
+                    echo "<p class='error'>$_SESSION[errorLogin]</p>";
+                    unset($_SESSION["errorLogin"]);
+                }
+            ?>
+        </form>
+    </body>
 </html>
